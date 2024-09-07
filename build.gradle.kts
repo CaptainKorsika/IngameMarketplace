8b19026a326d9426e8f43995f35eb0a48a4ed226
CaptainKorsika <Bastiprivate1686@gmail.com>
2024-09-07 19:40:01 +0200

    add dialect to hibernate

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation ("org.yank.sqlite:sqlite-dialect:1.0.0")
 }
 
 kotlin {

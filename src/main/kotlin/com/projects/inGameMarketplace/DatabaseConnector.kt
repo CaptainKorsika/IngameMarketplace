ddbd3b886a1c810dea7770f9f92ce9f49452495b
CaptainKorsika <Bastiprivate1686@gmail.com>
2024-09-08 13:08:45 +0200

    change to MySQL env variable

--- src/main/kotlin/com/projects/inGameMarketplace/DatabaseConnector.kt
+++ src/main/kotlin/com/projects/inGameMarketplace/DatabaseConnector.kt
@@ -4,17 +4,15 @@ import java.sql.Connection
 import java.sql.DriverManager
 
 class DatabaseConnector {
-    fun connectToDatabaseOld(): Connection {
-        val url = "jdbc:sqlite:E:src/main/resources/old_database.sql"
-        return DriverManager.getConnection(url)
-    }
+//    fun connectToDatabaseOld(): Connection {
+//        val url = "jdbc:sqlite:E:src/main/resources/old_database.sql"
+//        return DriverManager.getConnection(url)
+//    }
 
     fun connectToDatabase(): Connection {
         val url = "jdbc:mysql://localhost:3306/marketplace_database"
-        val user = "root"
-        val pw = "root"
+        val user = System.getenv("DB_USER")
+        val pw = System.getenv("DB_PW")
         return DriverManager.getConnection(url, user, pw)
     }
-
-
 }
